26a44c2fbea2b26e5dae57928cbeba81a270c33b
Guillaume <gthvn1@gmail.com>
2025-12-10 23:10:31 +0100

    Start the infinite loop
    Next, found how to handle ctrl-c

--- ethproxy/src/main.rs
+++ ethproxy/src/main.rs
@@ -1,12 +1,23 @@
-use ethproxy::setup;
+use std::io::{self, Write};
+//use ethproxy::setup;
 
-static VETHNAME: &str = "veth0";
-static VETHIP: &str = "192.168.35.1/24";
+//static VETHNAME: &str = "veth0";
+//static VETHIP: &str = "192.168.35.1/24";
 
 fn main() {
-    let veth = setup::Veth::init(VETHNAME, VETHIP);
+    //let veth = setup::Veth::init(VETHNAME, VETHIP);
+    //veth.create_device();
+    //veth.destroy_device();
+    println!("Ctrl-C to quit");
+    let mut input = String::new();
 
-    veth.create_device();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
 
-    veth.destroy_device();
+        print!("your input is {input}");
+    }
 }
