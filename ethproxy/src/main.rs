c7237743c7c7457d55c2b5a1d7fe77e4277c0aa6
Guillaume <guillaume.thouvenin@vates.tech>
2025-12-10 18:58:34 +0100

    Start playing with Command

--- ethproxy/src/main.rs
+++ ethproxy/src/main.rs
@@ -3,5 +3,6 @@ use ethproxy::setup;
 static VETHNAME: &str = "veth0";
 
 fn main() {
-    let _veth = setup::Veth::init(VETHNAME);
+    let veth = setup::Veth::init(VETHNAME);
+    veth.create_device();
 }
--- ethproxy/src/setup.rs
+++ ethproxy/src/setup.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 pub struct Veth {
     name: String,
     peer: String,
@@ -10,5 +12,19 @@ impl Veth {
             name: name.to_string(),
             peer,
         }
+    }
+
+    pub fn create_device(&self) {
+        // man 4 veth
+        // We need to run: ip link add <name> type veth peer name <peer>
+        let ip_args = ["link", "show", &self.name];
+        match Command::new("ip").args(ip_args).output() {
+            Ok(output) => {
+                println!("status: {}", output.status);
+                println!("stdout: {:?}", output.stdout);
+                println!("sderr : {:?}", output.stderr);
+            }
+            Err(error) => panic!("Probleme running ip: {error:?}"),
+        };
     }
 }
