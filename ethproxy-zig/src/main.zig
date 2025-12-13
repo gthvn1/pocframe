fc732e4376ca58d40c5ec82e5b596227d63c3eed
Guillaume <guillaume.thouvenin@vates.tech>
2025-12-13 20:46:28 +0100

    Read input from stdin

--- ethproxy-zig/src/main.zig
+++ ethproxy-zig/src/main.zig
@@ -9,4 +9,23 @@ pub fn main() !void {
 
     std.debug.print("Peer Interface: {s}\n", .{args.peer_iface});
     std.debug.print("Socket: {s}\n", .{args.socket});
+
+    // Read user input
+    var buffer: [1024]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().reader(&buffer);
+    const stdin = &stdin_reader.interface;
+
+    std.debug.print("Ctrl-d to end\n", .{});
+    std.debug.print("> ", .{});
+
+    while (stdin.takeDelimiterExclusive('\n')) |r| {
+        // need to toss the delimiter explicitly since we're not reading it
+        stdin.toss(1);
+
+        std.debug.print("You write <{s}>\n", .{r});
+        std.debug.print("> ", .{});
+    } else |_| {
+        std.debug.print("end", .{});
+        std.process.exit(1);
+    }
 }
